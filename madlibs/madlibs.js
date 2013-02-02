c085732e1c5ac5eb699023727d6510f4d7cd30db
Brad Sherwood <bcsherwood.nyc@gmail.com>
2013-02-02 17:51:11 -0500

    Week 1 homework
    madlibs

---
+++ madlibs/madlibs.js
@@ -0,0 +1,30 @@
+var div = document.getElementById("Madlib_Headline");
+div.innerHTML = "Brad's 1st Javascript MADLIBS!"
+
+var noun = document.getElementById('noun1');
+noun.value = "Enter a noun";
+
+var noun2= document.getElementById('noun2');
+noun2.value = "Enter a plural noun";
+
+var adjective = document.getElementById('adjective');
+adjective.value = "Enter an adjective";
+
+var verb = document.getElementById('verb');
+verb.value = "Enter a verb";
+
+var place = document.getElementById('place');
+place.value= "Enter a place";
+
+var madlib = document.getElementById('madlib');
+madlib.innerHTML="";
+
+
+var buttonInput = document.getElementById('submit');
+buttonInput.innerHTML="submit";
+
+buttonInput.onclick = function(){
+	madlib.innerHTML = "While out with " + noun.value +", <br>there was the most " + adjective.value + "<br>and inexplicable " + verb.value + ".<br>It reminded me of those " + noun2.value + "<br>that in the 80's were commonly seen at " + place.value +".";
+	buttonInput.innerHTML = "re-submit";
+	div.innerHTML = "DO it again?" 
+}
